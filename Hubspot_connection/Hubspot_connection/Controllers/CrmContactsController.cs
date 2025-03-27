6faa1b6f0e5f8eed2437c8e30422fb372bee7ad0
siddiquisabih <siddiquisabih94@gmail.com>
2025-03-27 11:55:05 +0500

    crm hubspot integration sample

---
+++ Hubspot_connection/Hubspot_connection/Controllers/CrmContactsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using HubSpot.NET.Core;
+using HubSpot.NET.Api.Contact.Dto;
+using HubSpot.NET.Core.Interfaces;
+using System.IO;
+using Microsoft.Extensions.Logging;
+namespace Hubspot_connection.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CrmContactsController : Controller
+    {
+
+
+
+        [HttpGet]
+        public async Task<List<ContactHubSpotModel>> GetAllContacts()
+        {
+            var hubSpotApi = new HubSpotApi("place Access Token Here");
+            try
+            {
+                var contacts = hubSpotApi.Contact.List<ContactHubSpotModel>();
+                return contacts.Contacts.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
+            }
+
+        }
+
+    }
+}
